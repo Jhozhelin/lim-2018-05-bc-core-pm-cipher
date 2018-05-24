c6d21f20104f95acaeb610a0a2fd54953c81f48f
Jhozhelin <jhozhelinzuniga@gmail.com>
2018-05-24 18:21:36 -0500

    agregar nuevos cambios

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
-var userName = prompt('Ingresa tu nombre :)');
+var userName = alert('Bienvenido :)');
 console.log(userName);
-if (userName!=" ") {
-  var userNameP = document.getElementById('userName');
-   userNameP.textContent =  userName.toUpperCase();
-}
-else {
-  var tituloID = document.getElementById('titulo');
-    tituloID.textContent = titulo + "usuario";
-}
-var choice = confirm ("¿List@ para jugar?");
-if (choice){
-
-}
